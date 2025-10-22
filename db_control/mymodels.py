2a707e14ad2d77f7c3b2bea9ddb8d7abcbe1389c
sawa <sawadaaa.aaa@gmail.com>
2025-10-23 03:14:19 +0900

    修正

--- db_control/mymodels.py
+++ db_control/mymodels.py
@@ -1,4 +1,4 @@
-from sqlalchemy import ForeignKey
+from sqlalchemy import ForeignKey, String, Integer
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from datetime import datetime
 
@@ -9,28 +9,30 @@ class Base(DeclarativeBase):
 
 class Customers(Base):
     __tablename__ = 'customers'
-    customer_id: Mapped[str] = mapped_column(primary_key=True)
-    customer_name: Mapped[str] = mapped_column()
-    age: Mapped[int] = mapped_column()
-    gender: Mapped[str] = mapped_column()
+    customer_id: Mapped[str] = mapped_column(String(10), primary_key=True)
+    customer_name: Mapped[str] = mapped_column(String(100))
+    age: Mapped[int] = mapped_column(Integer)
+    gender: Mapped[str] = mapped_column(String(10))
 
 
 class Items(Base):
     __tablename__ = 'items'
-    item_id: Mapped[str] = mapped_column(primary_key=True)
-    item_name: Mapped[str] = mapped_column()
-    price: Mapped[int] = mapped_column()
+    item_id: Mapped[str] = mapped_column(String(10), primary_key=True)
+    item_name: Mapped[str] = mapped_column(String(100))
+    price: Mapped[int] = mapped_column(Integer)
 
 
 class Purchases(Base):
     __tablename__ = 'purchases'
-    purchase_id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    purchase_name: Mapped[str] = mapped_column(ForeignKey("customers.customer_id"))
-    date: Mapped[datetime] = mapped_column()
+    purchase_id: Mapped[str] = mapped_column(String(10), primary_key=True)
+    customer_id: Mapped[str] = mapped_column(String(10), ForeignKey("customers.customer_id"))
+    purchase_date: Mapped[str] = mapped_column(String(10))
+    total_amount: Mapped[int] = mapped_column(Integer, default=0)
 
 
 class PurchaseDetails(Base):
     __tablename__ = 'purchase_details'
-    purchase_id: Mapped[int] = mapped_column(ForeignKey("purchases.purchase_id"), primary_key=True)
-    item_name: Mapped[str] = mapped_column(ForeignKey("items.item_id"), primary_key=True)
-    quantity: Mapped[int] = mapped_column()
+    detail_id: Mapped[str] = mapped_column(String(10), primary_key=True)
+    purchase_id: Mapped[str] = mapped_column(String(10), ForeignKey("purchases.purchase_id"))
+    item_id: Mapped[str] = mapped_column(String(10), ForeignKey("items.item_id"))
+    quantity: Mapped[int] = mapped_column(Integer)
