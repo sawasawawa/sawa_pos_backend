ebdd03f3b51df65ad591f70571da7ea06fbfff1e
sawa <sawadaaa.aaa@gmail.com>
2025-10-23 12:07:12 +0900

    Fix Azure App Service configuration for backend API

--- backend/app.py
+++ backend/app.py
@@ -27,8 +27,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "https://app-002-gen10-step3-1-node-oshima58.azurewebsites.net",
-        "https://*.azurewebsites.net"
+        "https://app-002-gen10-step3-1-node-oshima58.azurewebsites.net"
     ],
     allow_credentials=True,
     allow_methods=["*"],
